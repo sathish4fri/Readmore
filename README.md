ecb6e96c12fd8fe8de20dfe9ee0fa3e50186e610
sathishkumar <sathish4fri@gmail.com>
2015-01-19 21:37:45 +0530

    Readmore

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿OssnReadMore
+==========
+
+Readmore Component for Ossn
+
+Thanks to jeffoster for Nice Js .
+
+If you want setup some setting check the document with below link 
+
+https://github.com/jedfoster/Readmore.js
---
+++ ossn_com.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *    OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+//define('__ReadMore__', ossn_route()->com . 'ReadMore/');
+
+
+
+function ossn_ReadMore_init() {
+	ossn_new_js('jquery','components/Readmore/js/jquery-2');
+	ossn_load_js('jquery');
+	ossn_new_js('readmore', 'components/Readmore/js/readmoreorg');
+	ossn_load_js('readmore');
+   ossn_extend_view('js/opensource.socialnetwork', 'components/Readmore/js/readmore');
+}
+
+
+ossn_register_callback('ossn', 'init', 'ossn_ReadMore_init');
