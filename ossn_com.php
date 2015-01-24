3fad0edbf86ae89dd891e395d970e444946fcfd6
githubertus <github@spam-s.cked.de>
2015-01-24 15:33:02 +0100

    loading only 1 javascript

--- ossn_com.php
+++ ossn_com.php
@@ -13,13 +13,8 @@
 
 
 function ossn_ReadMore_init() {
-	ossn_new_js('jquery','components/Readmore/js/jquery-2');
-	ossn_load_js('jquery');
-	ossn_new_js('readmore', 'components/Readmore/js/readmoreorg');
-	ossn_load_js('readmore');
-	ossn_new_css('ossn.readmore', 'components/Readmore/css/readmore');
-	ossn_load_css('ossn.readmore');
-   ossn_extend_view('js/opensource.socialnetwork', 'components/Readmore/js/readmore');
+	ossn_extend_view('css/ossn.default', 'components/Readmore/css/readmore');
+	ossn_extend_view('js/opensource.socialnetwork', 'components/Readmore/js/needmore');
 }
 
 
