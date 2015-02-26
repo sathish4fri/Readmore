408ae6e2e3f93c6156905281d8a9bfb1954273e0
ctlui <ctmoi1@gmail.com>
2015-02-26 16:57:21 +0100

    Add French Language

---
+++ locale/ossn.fr.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *    OpenSource-SocialNetwork
+ *
+ * @package   (Informatikon.com).ossn
+ * @author    OSSN Core Team <info@opensource-socialnetwork.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://opensource-socialnetwork.com/licence
+ * @link      http://www.opensource-socialnetwork.com/licence
+ */
+$fr = array(
+    'module:readmore:read:more' => 'Lire plus',
+    'module:readmore:close' => 'Fermer',
+);
+ossn_register_languages('fr', $fr); 
